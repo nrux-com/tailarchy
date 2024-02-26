a15af3a0fa41acb0b225dbba18de4dc46628ebb9
Nataniel Regula <nataniel.regula17@gmail.com>
2024-02-26 19:41:15 +0000

    refactor: move decorationTypes to their own file

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,7 @@
 import * as vscode from 'vscode';
 import { ExtensionContext } from 'vscode';
 import classNamesCategories from './classNamesCategories';
-
-const decorationTypes = {
-  spacing: {
-    category: 'spacing',
-    decorations: {
-      subtle: vscode.window.createTextEditorDecorationType({
-        backgroundColor: 'rgba(0, 127, 244, 0.07)',
-        borderRadius: '7px',
-      }),
-      hivis: vscode.window.createTextEditorDecorationType({
-        backgroundColor: 'rgba(0, 127, 244, 0.07)',
-        borderRadius: '2px 2px 7px 7px',
-        borderColor: 'rgba(63, 114, 160, 0.9)',
-        borderWidth: '0px 2px 2px 2px',
-        borderStyle: 'solid',
-        // color: 'rgba(145, 198, 246, 1)',
-      }),
-    },
-  },
-};
+import decorationTypes from './decorationTypes';
 
 async function addHighlights() {
   const activeTextEditor = vscode.window.activeTextEditor;
